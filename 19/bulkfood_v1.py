c476914fde58eb37e170016143c0fc9c611b6a71
gaochangju <gaochangju95@163.com>
2021-03-21 14:10:30 +0800

    @property特性解析，验证、属性接口隐藏

--- 19/bulkfood_v1.py
+++ 19/bulkfood_v1.py
@@ -1,5 +1,5 @@
 class LineItem:
-    def __init__(self,description,weight,price):
+    def __init__(self, description, weight, price):
         self.description = description
         self.weight = weight
         self.price = price
@@ -10,4 +10,6 @@ class LineItem:
 
 if __name__ == '__main__':
     raisins = LineItem('Golden raisins', 10, 6.95)
-    print(raisins.subtotal())
+    print(raisins.subtotal())  # 输出 69.5
+    raisins.weight = -10
+    print(raisins.subtotal())  # 输出 -69.5
--- 19/bulkfood_v2.py
+++ 19/bulkfood_v2.py
@@ -8,15 +8,33 @@ class LineItem:
         self.price = price
 
     def subtotal(self):
+        """this is help doc"""
         return self.weight * self.price
 
     @property
     def weight(self):
-        return self.__weight
+
+        return self.__dict__['weight']
 
     @weight.setter
     def weight(self, value):
         if value > 0:
+            # self.__weight = value
             self.__weight = value
+            # self.__dict__['weight'] = value
         else:
             raise ValueError('value must be > 0')
+
+    @weight.getter
+    def weight(self):
+        """this is help doc for weight attr"""
+        return self.__weight * 10
+
+
+if __name__ == '__main__':
+    raisins = LineItem('Golden raisins', 10, 6.95)
+    print(raisins.subtotal())  # 输出 69.5
+    # raisins.weight = -10       # ValueError: value must be > 0
+    print(raisins.subtotal())
+    print(raisins.weight)
+    help(LineItem)
